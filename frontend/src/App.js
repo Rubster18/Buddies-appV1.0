d0678989ef351332c621dd9a7250edefa291f020
Rubén Adarme <rubenadarme@gmail.com>
2020-09-23 15:50:11 +0200

    front end ready

---
+++ frontend/src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import "./styles/directoring.scss";
+import BuddiesForm from "./BuddiesForm";
+import StudentForm from "./StudentForm"
+import Homepage from './Homepage';
+import ThankYou from './ThankYou';
+import Login from './Login';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import AdminTable from './AdminTable';
+
+//Main Site
+function App(){
+  return(
+    <div className="App">
+        
+        <Router>
+        <Switch>
+            <Route exact path="/">
+              <Homepage />
+            </Route>
+            <Route path="/BuddiesForm">
+              <BuddiesForm/>
+            </Route>
+            <Route path="/StudentForm">
+              <StudentForm/>
+            </Route>
+            <Route path="/ThankYou">
+              <ThankYou/>
+            </Route>
+            <Route path="/Login">
+              <Login/>
+            </Route>
+            <Route path="">
+              <AdminTable/>
+            </Route>
+          </Switch>
+      </Router>
+    </div>
+  );
+}
+export default App;
+
+
+
+
